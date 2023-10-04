aff6f57eaeb0e40a92df076c3d1425e2d29b1ebd
Oscar Svensson <oscar.svensson23@gmail.com>
2023-10-04 15:40:30 +0200

    working code with time and temp

--- hub/main.py
+++ hub/main.py
@@ -1,8 +1,10 @@
 import paho.mqtt.client as mqtt
 import serial
 import json
+import time
+import struct
 
-with open("config.json") as config_file:
+with open("hub/config.json") as config_file:
     config_data = json.load(config_file) 
   
 name = config_data.get("name")
@@ -16,16 +18,24 @@ client.connect(broker, port=1883, keepalive=60)
 # Start a background loop that handles all broker communication
 client.loop_start()
 
-with serial.Serial("COM3", 9600, timeout=1) as ser:
+with serial.Serial("COM3", 115200, timeout=1) as ser:
     while True:
         line = ser.readline()   # read a '\n' terminated line
+        ts = time.time()
+        timeInt = int(ts)
         if len(line) != 0:
             str = line.decode("utf-8")
-            mystring = str.split()
-            part1 = mystring[0]
-            part2 = mystring[1]
-            tippelitopic = topic/name/part1/part2
+            list_ = str.split()
+            pico_id = list_[0]
+            temp_id = list_[1]
+            temp = float(list_[2])
+            timeInt = int(ts)
             
-            msg = client.publish("tippelitopic", payload=f"Oscar: {line}", qos=1) 
+            TempTrunk = int(temp*1000)
+            RealTopic = f"{topic}/{name}/{pico_id}/{temp_id}"
+            
+            payload = struct.pack(">Ii", timeInt, TempTrunk)
+            
+            msg = client.publish(RealTopic, payload=payload, qos=1) 
             msg.wait_for_publish()
 
